89530682c517b1912d44c854592f2554e6be04b2
Nasib-Nur <Nasib.Nur99k@outlook.com>
2025-01-22 11:06:23 +0200

    digi-advertising website

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"]
-  }
+    
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   
 };
 
